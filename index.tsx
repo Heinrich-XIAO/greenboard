ca7cb8a713389a2314c77776842af703a4c5b166
Heinrich Xiao <74563446+Heinrich-XIAO@users.noreply.github.com>
2025-10-02 19:53:36 -0400

    connect things to micromod that are not just power rails

--- index.tsx
+++ index.tsx
@@ -2,13 +2,12 @@ import GreenpillModule from "@tsci/Heinrich-XIAO.greenpill"
 import { MicroModBoard } from "@tscircuit/common"
 
 export default () => (
-  <group>
+  <breakout>
     <MicroModBoard
       name="micromod"
       connections={{
         GND2: "net.GND"
       }}
-      schX={20}
     >
     </MicroModBoard>
     <trace
@@ -24,6 +23,26 @@ export default () => (
       pcbY={-1}
       pcbX={0}
       resetButton={false}
+      schX={20}
+      PC0={[".micromod > .SPI_N_CS"]}
+      PC1={[".micromod > .I2C_SDA1"]}
+      PC2={[".micromod > .I2C_SCL1"]}
+      PC3={[".micromod > .G2"]}
+      PC4={[".micromod > .G3"]}
+      PC5={[".micromod > .SPI_SCK"]}
+      PC6={[".micromod > .SPI_SDO1"]}
+      PC7={[".micromod > .SPI_SDI1"]}
+      PD2={[".micromod > .G7"]}
+      PD3={[".micromod > .G8"]}
+      PD4={[".micromod > .A0"]}
     />
-  </group>
+    <trace
+      from="net.SWIO"
+      to=".micromod > .SWDIO"
+    />
+    <trace
+      from="net.RST"
+      to=".micromod > .N_RESET"
+    />
+  </breakout>
 )
