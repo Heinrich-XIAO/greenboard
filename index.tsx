bdaf05d257fbfe2f9e27cd385278957ecce05e22
Heinrich Xiao <74563446+Heinrich-XIAO@users.noreply.github.com>
2025-10-02 18:01:33 -0400

    micromod gold finger offset fixed, connected all power

--- index.tsx
+++ index.tsx
@@ -2,18 +2,28 @@ import GreenpillModule from "@tsci/Heinrich-XIAO.greenpill"
 import { MicroModBoard } from "@tscircuit/common"
 
 export default () => (
-  <MicroModBoard name="micromod"
-    connections={{
-      GND2: "net.GND"
-    }}
-    schX={20}
-  >
+  <group>
+    <MicroModBoard
+      name="micromod"
+      connections={{
+        GND2: "net.GND"
+      }}
+      schX={20}
+    >
+    </MicroModBoard>
+    <trace
+      from="net.VDD"
+      to=".micromod > .V3_3_2"
+    />
+    <trace
+      from="net.GND"
+      to=".micromod > .GND2"
+    />
     <GreenpillModule
       name="a"
-      pcbY={0}
-      pcbX={-1}
+      pcbY={-1}
+      pcbX={0}
       resetButton={false}
     />
-    <trace from="net.GND" to=".micromod > .GND2" />
-  </MicroModBoard>
+  </group>
 )
